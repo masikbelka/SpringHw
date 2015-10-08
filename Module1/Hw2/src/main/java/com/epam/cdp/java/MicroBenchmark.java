e6f74aa6ccdcfc6d72c12212755b11c8ad5ba9ad
Tetiana Mokhnenko <tmokhnenko@blackberry.com>
2015-10-08 20:19:05 +0300

    HW2 init commit

---
+++ Module1/Hw2/src/main/java/com/epam/cdp/java/MicroBenchmark.java
@@ -0,0 +1,41 @@
+package main.java.com.epam.cdp.java;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+import java.util.function.Supplier;
+
+import static java.util.stream.Collectors.toList;
+
+public class MicroBenchmark {
+
+    private static final int TEST_COUNT = 5;
+
+    public static void main(String[] args) throws IOException {
+        List<String> words = loadWords();
+        test("Task 1, java 7, serial ", () -> Tasks.task1Java7(words));
+//        test("Task 1, java 8, serial ", () -> Tasks.task1Java8(words, false));
+//        test("Task 1, java 8, parallel ", () -> Tasks.task1Java8(words, true));
+//        test("Task 2, java 7, serial ", () -> Tasks.task2Java7(words));
+//        test("Task 2, java 8, serial ", () -> Tasks.task2Java8(words, false));
+//        test("Task 1, java 8, parallel ", () -> Tasks.task2Java8(words, true));
+        //NOTE: as this is a microbenchmark, please run tests separately.
+    }
+
+    public static <T> void test(String label, Supplier<T> task) {
+        for (int i = 0; i < TEST_COUNT; i++) {
+            System.out.printf("%s: time - %d\n", label, oneRun(task));
+        }
+    }
+
+    public static <T> long oneRun(Supplier<T> task) {
+        long time = System.nanoTime();
+        task.get();
+        return System.nanoTime() - time;
+    }
+
+    private static List<String> loadWords() throws IOException {
+        return Files.lines(Paths.get("samples/wordslist.txt")).collect(toList());
+    }
+}
---
+++ Module1/Hw2/src/main/java/com/epam/cdp/java/Tasks.java
@@ -0,0 +1,37 @@
+package main.java.com.epam.cdp.java;
+
+import java.nio.file.Path;
+import java.util.List;
+import java.util.regex.Pattern;
+
+public class Tasks {
+
+    private static final Pattern ALPHABETIC_RPL = Pattern.compile("[^a-zA-Z]");
+    // ALPHABETIC_RPL.matcher(w).replaceAll("")
+
+    public static List<String> task1Java7(List<String> words) {
+        //TODO your code here
+        return null;
+    }
+
+    public static List<String> task1Java8(List<String> words, boolean isParallel) {
+        //TODO your code here
+        return null;
+    }
+
+    public static List<String> task2Java7(List<String> words) {
+        //TODO your code here
+        return null;
+    }
+
+    public static List<String> task2Java8(List<String> words, boolean isParallel) {
+        //TODO your code here
+        return null;
+    }
+
+    public static List<String> task3(Path... paths) {
+        //TODO your code here
+        return null;
+    }
+
+}
